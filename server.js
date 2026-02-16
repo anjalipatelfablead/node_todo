99bf4a4c69bf6f0c0f439d2d24e9463343531ab6
anjalipatelfablead <anjalipatel.fablead@gmail.com>
2026-02-16 17:10:59 +0530

    added multure for taking form data

--- server.js
+++ server.js
@@ -1,20 +1,25 @@
 const express = require('express');
 require("dotenv").config();
-const cors= require("cors");
+const cors = require("cors");
+const multer = require("multer");
 
-const db= require("./dbconfig/db");
-const userroute= require("./routes/user");
-const taskroute= require("./routes/task");
+const db = require("./dbconfig/db");
+const userroute = require("./routes/user");
+const taskroute = require("./routes/task");
 
-const app= express();
+const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 db();
 
-app.use("/user",userroute);
-app.use("/task",taskroute);
+const upload = multer();
+app.use(upload.none()); // for text-only multipart form-data
 
-app.listen(process.env.PORT,()=>{
+app.use("/user", userroute);
+app.use("/task", taskroute);
+
+app.listen(process.env.PORT, () => {
     console.log("listening on port 3000");
 });
