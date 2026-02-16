784103f373d33c575a34638548e290ab6269f899
anjalipatelfablead <anjalipatel.fablead@gmail.com>
2026-02-16 13:51:48 +0530

    added auth varification for by token

--- middleware/roleMiddleware.js
+++ middleware/roleMiddleware.js
@@ -1,14 +1,32 @@
+// const roleMiddleware = (allowedRoles) => {
+//     return (req, res, next) => {
+
+//         const userRole = req.headers.role; 
+
+//         if (!userRole) {
+//             return res.status(403).json({ message: "Role not provided" });
+//         }
+
+//         if (!allowedRoles.includes(userRole)) {
+//             return res.status(403).json({ message: "Access denied" });
+//         }
+
+//         next();
+//     };
+// };
+
+// module.exports = roleMiddleware;
+
+
 const roleMiddleware = (allowedRoles) => {
     return (req, res, next) => {
 
-        const userRole = req.headers.role; 
-
-        if (!userRole) {
-            return res.status(403).json({ message: "Role not provided" });
+        if (!req.user) {
+            return res.status(401).json({ message: "Unauthorized user" });
         }
 
-        if (!allowedRoles.includes(userRole)) {
-            return res.status(403).json({ message: "Access denied" });
+        if (!allowedRoles.includes(req.user.role)) {
+            return res.status(403).json({ message: "Forbidden: Access denied" });
         }
 
         next();
--- routes/task.js
+++ routes/task.js
@@ -1,14 +1,17 @@
-const express= require("express");
-const {addtask, getalltask, gettaskbyid ,updatetask,deletetask,gettaskbyuser} = require("../controllers/task");
+const express = require("express");
+const { addtask, getalltask, gettaskbyid, updatetask, deletetask, gettaskbyuser } = require("../controllers/task");
 const roleMiddleware = require("../middleware/roleMiddleware");
+const authMiddleware = require("../middleware/authMiddleware");
 
 const router = express.Router();
 
-router.post("/", roleMiddleware(["admin", "user"]),addtask);
-router.get("/",roleMiddleware(["admin"]),getalltask);
-router.get("/user/:id",gettaskbyuser);
-router.get("/:id",gettaskbyid);
-router.put("/:id",updatetask);
-router.delete("/:id",deletetask);
+router.post("/", authMiddleware, roleMiddleware(["admin", "user"]), addtask);
+router.get("/", authMiddleware, roleMiddleware(["admin"]), getalltask);
+// router.post("/", addtask);
+// router.get("/", getalltask);
+router.get("/user/:id", authMiddleware, roleMiddleware(["admin", "user"]), gettaskbyuser);
+router.get("/:id", authMiddleware, roleMiddleware(["admin", "user"]), gettaskbyid);
+router.put("/:id", authMiddleware, roleMiddleware(["admin", "user"]), updatetask);
+router.delete("/:id", authMiddleware, roleMiddleware(["admin", "user"]), deletetask);
 
-module.exports= router;
+module.exports = router;
