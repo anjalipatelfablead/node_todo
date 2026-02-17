c0e5e57f6a5d011536fdd499d3a06350cf7e8641
anjalipatelfablead <anjalipatel.fablead@gmail.com>
2026-02-17 11:50:29 +0530

    updated response msg

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -15,7 +15,7 @@ const authMiddleware = (req, res, next) => {
         req.user = decoded; 
         next();
     } catch (error) {
-        return res.status(401).json({ message: "Invalid or expired token" });
+        return res.status(401).json({ message: "User not logged in / Invalid or expired token" });
     }
 };
 
